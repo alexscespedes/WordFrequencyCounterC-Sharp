7f943fd5cdb33b31710945ccb670106f00a65bae
alexscespedes <alexandersenc99@gmail.com>
2025-06-07 10:06:51 -0400

    Normalize Input

--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
-﻿namespace WordFrequencyCounter;
+﻿using System.Text.RegularExpressions;
+
+namespace WordFrequencyCounter;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter a sentence:");
+        string input = Console.ReadLine()!;
+
+        string cleanedInput = NormalizeText(input);
+
+        string[] words = cleanedInput.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var word in words)
+        {
+            Console.WriteLine(word);
+        }
     }
+
+    static string NormalizeText(string text)
+    {
+        text = text.ToLower();
+
+        text = Regex.Replace(text, @"[^\w\s]", "");
+
+        return text;
+    } 
 }
